dab7e5c3d154026ef6160865d85415a814385c45
Joaquim Sancha <japsancha@cvtelecom.cv>
2020-08-31 15:39:00 -0100

    add jest

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testEnvironment: 'node'
+}
