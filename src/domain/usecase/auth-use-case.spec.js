4ce0cf3f9e982c7c67616b84bbba3e41a0d89199
Joaquim Sancha <japsancha@cvtelecom.cv>
2020-09-12 18:19:06 -0100

    ensure AuthUseCase throws MissingParamError if no email is provided

--- src/domain/usecase/auth-use-case.spec.js
+++ src/domain/usecase/auth-use-case.spec.js
@@ -1,7 +1,8 @@
+const { MissingParamError } = require('../../utils/errors')
 class AuthUseCase {
   async auth (email) {
     if (!email) {
-      return new Error()
+      return new Error(new MissingParamError('email'))
     }
   }
 }
@@ -10,6 +11,6 @@ describe('Auth UseCase', () => {
   test('should return null if no email is provided', async () => {
     const sut = new AuthUseCase()
     const promise = sut.auth()
-    expect(promise).rejects.toThrow()
+    expect(promise).rejects.toThrow(new MissingParamError('email'))
   })
 })
